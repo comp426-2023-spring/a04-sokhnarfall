37addaad4cf75d571f62d22a6e1f8be5c0276244
Sokhna Fall <84427104+sokhnarfall@users.noreply.github.com>
2023-04-20 15:09:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
+#!/usr/bin/env node
+
 import { rps, rpsls } from "./lib/rpsls.js"
 import minimist from "minimist"
-import express, { json } from "express"
+import express from "express"
 
 var argv = minimist(process.argv.slice(2));
 var app = express()
+const port = argv.port || 5000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(json());
-const port = argv.port || 5000
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/app', (req, res)=>{
-    res.status(200).send("200 OK");
+app.get('/app/', (req, res) => {  
+    res.status(200).send("200 OK")
 })
 
-app.get('/app/rps/', (req, res) => {  
+app.get('/app/rps/', (req, res) => { 
     res.status(200).send(rps(null));
 })
 
-app.get('/app/rpsls/', (req, res) => {   
+app.get('/app/rpsls/', (req, res) => {
     res.status(200).send(rpsls(null));
 })
 
 app.get('/app/rps/play/', (req, res) => { 
-    res.status(200).send(rps(req.query.action));
+    res.status(200).send(rps(req.query.shot));
 })
 
-app.post('/app/rps/play/', (req, res) => {
-    res.status(200).send(rps(req.body.action));
+app.post('/app/rps/play/', (req, res) => { 
+    res.status(200).send(rps(req.body.shot));
 })
 
-app.get('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(rpsls(req.query.action));
+app.get('/app/rpsls/play/', (req, res) => { 
+    res.status(200).send(rpsls(req.query.shot));
 })
 
 app.post('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(rpsls(req.body.action));
+    res.status(200).send(rpsls(req.body.shot));
 })
 
 app.get('/app/rps/play/:shot/', (req, res) => {
-    res.status(200).send(rps(req.params.action));
+    //let shot = parseInt(req.params.shot)
+    res.status(200).send(rps(req.params.shot));
 })
 
 app.get('/app/rpsls/play/:shot/', (req, res) => {
-    res.status(200).send(rpsls(req.params.action));
+    //let shot = parseInt(req.params.shot)
+    res.status(200).send(rpsls(req.params.shot));
 })
 
 app.get('*', (req, res) => {
